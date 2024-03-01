2314d76598cbfd641057eb709e04937fb0660628
prafullprince <prafullprince12@gmail.com>
2024-03-01 23:01:13 +0530

    complete Authentication

--- 03AuthNAuthR/controllers/AuthN.js
+++ 03AuthNAuthR/controllers/AuthN.js
@@ -1,5 +1,8 @@
 const User = require("../models/AuthModels");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 
 exports.userSignUpAuthn = async (req,res)=>{
     try{
@@ -34,3 +37,61 @@ exports.userSignUpAuthn = async (req,res)=>{
         throw new Error("somethin went wrogn");
     }
 };
+
+exports.userLoginAuthn = async (req,res)=>{
+    try{
+        // fetch data from users
+        const {email,password} = req.body;
+        // validation of data
+        if(!email || !password){
+            return res.json({
+                success:false,
+                message:"please fill all details",
+            });
+        }
+        // check user is correct or not
+        let existingUser = await User.findOne({email});
+        if(!existingUser){
+            return res.json({
+                success:false,
+                message:"user is not available",
+            });
+        }
+        // check password is matched or not
+        // we use bcrypt.compare(1,2) -> it takes 2 arguments = 1>> req.body wala password & 2>> jouser database mn hai uss user ka password
+        let passVerification = await bcrypt.compare(password, existingUser.password);
+        // making payload(user ka data)
+        const payload = {
+            email:existingUser.email,
+            id:existingUser._id,
+        }
+        if(passVerification){
+            // password match kr gaya === jwt client/user ko dedo
+            let token = jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:"2h"});
+            existingUser = existingUser.toObject();
+            existingUser.token = token;
+            // we have to protect our user password
+            existingUser.password = undefined; 
+            // create options
+            const options = {
+                expiresIn: new Date(Date.now()+3*24*60*60*1000),
+                httpOnly:true,
+            }
+            // now create cookie and send data using cookie
+            res.cookie("princeCookie",token,options).status(200).json({
+                success:true,
+                token,
+                existingUser,
+                message:"a user logged in successfully"
+            });
+        }else{
+            // password match nhi hua
+            return res.json({
+                success:false,
+                message:"Password Incorrect",
+            })
+        }
+    }catch(error){
+        console.log(error);
+    }
+}
--- 03AuthNAuthR/routes/Route.js
+++ 03AuthNAuthR/routes/Route.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+
 // import controllers
 const {userSignUpAuthn} = require("../controllers/AuthN");
+const {userLoginAuthn} = require("../controllers/AuthN");
+
+
 // making routes
 router.post("/signup",userSignUpAuthn);
+router.post("/login",userLoginAuthn);
+
+
 // export
 module.exports = router;
